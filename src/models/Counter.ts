7cd95380c0837e3215fc2f01951387cddb043b16
Thant Zin Lin <tzlin@mit.com.mm>
2024-12-07 16:00:26 +0630

    fix error

--- src/models/Counter.ts
+++ src/models/Counter.ts
@@ -1,13 +1,13 @@
 import mongoose, { Schema, Document } from "mongoose";
-export interface Counter extends Document {
+export interface ICounter extends Document {
   date: string;
   sequence: number;
 }
 
-const CounterSchema = new Schema<Counter>({
+const CounterSchema = new Schema<ICounter>({
   date: { type: String, required: true, unique: true },
   sequence: { type: Number, default: 0 },
 });
 
-const Counter = mongoose.model<Counter>("Counter", CounterSchema);
+const Counter = mongoose.model<ICounter>("Counter", CounterSchema);
 export default Counter;
