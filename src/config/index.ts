8b16ecbf488d363acc31e387747f1506177008b4
Thant Zin Lin <tzlin@mit.com.mm>
2024-12-07 22:43:15 +0630

    update port

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const config = {
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/ecommerce",
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 8080,
   redisUri: process.env.REDIS_URI || "redis://localhost:6379",
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   SENDER_EMAIL: process.env.SENDER_EMAIL,
