3726e061af095ecac767a704be7d03c7dae5ea45
Thant Zin Lin <tzlin@mit.com.mm>
2024-12-07 22:26:20 +0630

    update

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,8 +6,6 @@ const router = Router();
 
 router.get("/", isAuth, userController.getUsers);
 router.get("/:id", isAuth, userController.getUser);
-// router.post("/", userController.createUser);
 router.put("/:id", isAuth, userController.updateUser);
-// router.delete("/:id", isAuth, userController.deleteUser);
 
 export default router;
