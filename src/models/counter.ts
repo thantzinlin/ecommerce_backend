d515e4f9ae7d4855dada7ddccae589660e5251df
Thant Zin Lin <tzlin@mit.com.mm>
2024-12-08 14:13:57 +0630

    update

---
+++ src/models/counter.ts
@@ -0,0 +1,12 @@
+import mongoose, { Schema, Document } from "mongoose";
+export interface ICounter extends Document {
+  date: string;
+  sequence: number;
+}
+
+const CounterSchema = new Schema<ICounter>({
+  date: { type: String, required: true, unique: true },
+  sequence: { type: Number, default: 0 },
+});
+
+export const Counter = mongoose.model<ICounter>("Counter", CounterSchema);
