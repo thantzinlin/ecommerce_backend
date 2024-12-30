d2aab44685bb75e51773f87c3a6f9f15a75bdde3
Thant Zin Lin <tzlin@mit.com.mm>
2024-12-30 21:47:32 +0630

    update

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -3,11 +3,12 @@ import * as categoryController from "../controllers/categoryController";
 import isAuth from "../middleware/authMiddleware";
 
 const router = Router();
-
+router.get("/getall", isAuth, categoryController.getALLForAdmin);
 router.get("/", categoryController.getALL);
 router.get("/:id", categoryController.getById);
+router.get("/:slug", categoryController.getBySlug);
 
-router.get("/:id", categoryController.getById);
+//router.get("/:id", categoryController.getById);
 
 router.post("/", isAuth, categoryController.create);
 router.put("/:id", isAuth, categoryController.findByIdAndUpdate);
